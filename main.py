8dd64192be55088b13385140384cc8b331feae1b
fdhd <michal.mariusz.lewandowski@gmail.com>
2022-03-16 16:44:05 +0100

    added bayesian methods

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import datetime
 from pathlib import Path
 from arg_parser import main_args
 from src.data.make_data import create_burr_data
+import matplotlib.pyplot as plt
 
 
 def compute_true_quantiles(quantile_levels: list, burr_beta: float, burr_lambda: float, burr_tau: float) -> list:
@@ -35,6 +36,37 @@ def k_greatest_values_matrices(input_matrix: numpy.ndarray, n_excesses: int) -> 
 
 def estimate_quantiles_frequentist_methods(args, burr_data, n_excesses_):
     from src.frequentist_methods import freq_methods
+    # How many values do we want to consider as excesses. The more, the better approximation should we obtain
+
+    keep_quantiles = {'pwm_gpd': np.zeros((len(args.quantile_levels), n_excesses_.shape[0])).T,
+                      'mom_gpd': np.zeros((len(args.quantile_levels), n_excesses_.shape[0])).T,
+                      'mom_fisher': np.zeros((len(args.quantile_levels), n_excesses_.shape[0])).T,
+                      'mle_gpd': np.zeros((len(args.quantile_levels), n_excesses_.shape[0])).T}
+
+    for ind, n_excesses in enumerate(n_excesses_):  # for different number of excesses
+        shifted_excesses, thresholds = k_greatest_values_matrices(burr_data, n_excesses)
+
+        qnts = np.zeros((len(keep_quantiles), len(args.quantile_levels)))
+
+        for excesses, threshold in zip(shifted_excesses, thresholds):
+            frequentist = freq_methods(data=burr_data, quantile_levels=args.quantile_levels,
+                                       excesses=excesses, thresholds=thresholds)
+            # fit GPD and Fisher distributions to excesses from each dataset
+            qnts[0] += frequentist.PWM_GPD()
+            qnts[1] += frequentist.MOM_GPD()  #
+            qnts[2] += frequentist.MLE_GPD()
+            qnts[3] += frequentist.MOM_Fisher()
+
+        keep_quantiles.get('pwm_gpd')[ind] = qnts[0] / args.n_different_samples
+        keep_quantiles.get('mom_gpd')[ind] = qnts[1] / args.n_different_samples
+        keep_quantiles.get('mle_gpd')[ind] = qnts[2] / args.n_different_samples
+        keep_quantiles.get('mom_fisher')[ind] = qnts[3] / args.n_different_samples
+
+    return keep_quantiles
+
+
+def estimate_quantiles_bayesian_methods(args, burr_data, n_excesses_):
+    from src.bayes_methods import bayes_methods
     # How many values do we want to consider as excesses. The more, the better approximation should we obtain
 
     keep_quantiles = {'pwm_gpd': np.zeros((len(args.quantile_levels), n_excesses_.shape[0])).T,
@@ -136,13 +168,15 @@ def plot(keep_quantiles:dict):
 
 
 def main(args, n_excesses_):
+    import matplotlib.pyplot as plt
     # load data
     save_dir = Path.cwd() / 'src/data/data_burr/'
     file_name = save_dir.__str__() + f'\\beta_{args.burr_beta}__tau_{args.burr_tau}__lambda_{args.burr_lambda}__n_samples_{args.n_different_samples}__n_obs_per_sample_{args.n_points_each_sample}.npy'
+
     with open(file_name, 'rb') as f:
         burr_data = np.load(f)
-    import matplotlib.pyplot as plt
-    # take a random sample
+
+    # take a random sample for plotting and computing first two moments
     ind = int(np.random.uniform(burr_data.shape[0], size=1))
     plt.hist(burr_data[ind], bins='auto')
     plt.title(f'Empirical mean {round(np.mean(burr_data[ind]),3)}, '
@@ -150,6 +184,8 @@ def main(args, n_excesses_):
     plt.show()
 
     keep_quantiles = estimate_quantiles_frequentist_methods(args, burr_data, n_excesses_)
+    # todo here get the bayesian quantile estimates
+
     # qnts_pwm_gpd, qnts_mom_gpd, qnts_mom_fisher, qnts_mle_gpd
     # columns: different nb of excesses used to estimate quantiles
     # rows: quantiles of different levels
@@ -185,7 +221,14 @@ def main(args, n_excesses_):
     # todo: put on the legend which color corresponds to which quantile level
     #  split keys on '_' for naming and use capital letters
     #  denote on x axis how many quantiles were used
+
+    f = lambda m, c: plt.plot([], [], marker=m, color=c, ls="none")[0]
+
+    handles = [f("s", colors[i]) for i in range(3)]
+    handles += [f(markers[i], "k") for i in range(3)]
+
     legend_without_duplicate_labels(ax)
+
     plt.show()
 
     # now for testing how good the fit is
@@ -208,7 +251,8 @@ if __name__ == '__main__':
     print('Starting')
     args = main_args()
     n_excesses_ = np.linspace(args.min_nb_obs_considered_excesses,
-                              args.max_nb_obs_considered_excesses, args.n_different_excesses).astype(int)
+                              args.max_nb_obs_considered_excesses,
+                              args.n_different_excesses).astype(int)
 
     main(args, n_excesses_)
     ending = datetime.datetime.now()
